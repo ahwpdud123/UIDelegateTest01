ed40b97249bf11e7981c5769f3383d7e334bf659
D7703_29 <d7703_29@d7703-29ui-imac.local>
2018-03-22 11:48:50 +0900

    UITextFieldDelegate01

--- UIDelegateTest01/ViewController.swift
+++ UIDelegateTest01/ViewController.swift
@@ -10,16 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var resultLabel: UILabel!
+    @IBOutlet var myTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        myTextField.placeholder = "입력해라!"
+        myTextField.clearButtonMode = UITextFieldViewMode.whileEditing
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        resultLabel.text = "Hello" + " " + myTextField.text!
+        myTextField.text = ""
+        myTextField.resignFirstResponder()
+        
+        
+        
+        
+    
 }
 
+}
